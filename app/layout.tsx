22c6e5a72cbe55a8435b8b8ed99525fe3abbcc51
masterBekshoh <bekshohdeveloper@gmail.com>
2024-07-05 16:49:22 +0500

    default folder and all page UI

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ChildProps } from '@/types'
+import type { Metadata } from 'next'
+import { Crete_Round, Work_Sans } from 'next/font/google'
+import './globals.css'
+
+const creteRound = Crete_Round({
+	weight: ['400'],
+	subsets: ['latin'],
+	variable: '--font-creteRound',
+})
+const workSans = Work_Sans({
+	weight: ['500', '600'],
+	subsets: ['latin'],
+	variable: '--font-workSans',
+})
+
+export const metadata: Metadata = {
+	title: 'Sammi dasturlashga oid maqolalar',
+	description:
+		'Dasturlash haqida yangiliklar, maslahatlar, va dasturlash sohasidagi eng soʻnggi xabarlar. Bizning blogda dasturlashni oʻrganish va rivojlantirish uchun qoʻllanma topishingiz mumkin.',
+}
+
+function RootLayout({ children }: ChildProps) {
+	return (
+		<html lang='en' suppressHydrationWarning>
+			<body
+				className={`${creteRound.variable} ${workSans.variable} overflow-x-hidden`}
+			>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	)
+}
+
+export default RootLayout
